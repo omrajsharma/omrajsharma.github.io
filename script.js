8bfe8a12a9a2590dc50706e514917ecc11f0d1da
omrajsharma <omraj7413@gmail.com>
2024-02-04 10:25:57 +0530

    feat: to top

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ window.onscroll = function () {
     const currentScrollHeight = document.documentElement.scrollTop;
     const scrollHeightPercentage = (currentScrollHeight / totalHeight) * 100;
     document.getElementById('scroll-bar').style.width = scrollHeightPercentage + '%';
+    const toTopButton = document.getElementById('to-top')
+    if (currentScrollHeight > 400) {
+        toTopButton.style.display = 'flex';
+    } else {
+        toTopButton.style.display = 'none';
+    }
+}
+function scrollToTop() {
+    window.scrollTo(0,0)
 }
